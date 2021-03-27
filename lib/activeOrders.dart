df6604f13164cd6cb13afc548bee83ff72c018ff
Laxmi Mutakekar <laxmimutakekar@gmail.com>
2021-03-27 15:55:25 +0530

    second commit

--- lib/activeOrders.dart
+++ lib/activeOrders.dart
@@ -1,74 +1,76 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:vertical/categorySelector.dart';
-import 'package:sellerapp/orders.dart';
-class CategoriesScroller extends StatelessWidget {
-  const CategoriesScroller();
-  Future<List<Orders>> fetchItems (BuildContext context) async{
-    final jsonString=await DefaultAssetBundle.of(context).loadString('assets/orders.json');
-    return ordersFromJson(jsonString);
+import 'package:sellerapp/models/orders.dart';
+import 'package:http/http.dart' as http;
+
+class ActiveOrders extends StatelessWidget {
+  const ActiveOrders();
+  Future<List<Orders>> fetchItems(BuildContext context) async {
+ 
+  final response = await http.get(Uri.parse("http://10.0.2.2:8080/orders/seller/1"));
+
+  if (response.statusCode == 200) {
+    return ordersFromJson(response.body);
+  } else {
+    throw Exception();
   }
+}
+
   @override
   Widget build(BuildContext context) {
     final double categoryHeight = MediaQuery.of(context).size.height * 0.30 - 50;
     return SingleChildScrollView(
       physics: BouncingScrollPhysics(),
-      scrollDirection: Axis.horizontal,
+      scrollDirection: Axis.vertical,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
 
           Container(
-            margin: EdgeInsets.only(right: 20),
-            height: 150,
-            decoration: BoxDecoration(color: Colors.white),
+            margin: EdgeInsets.only(right: 5),
+            height: 400,
             child: FutureBuilder(
+            
               future: fetchItems(context),
               builder: (context,snapshot){
                 if (snapshot.hasData){
                   return ListView.builder(
-                    scrollDirection: Axis.horizontal,
+                    scrollDirection: Axis.vertical,
                     itemCount: snapshot.data.length,
                     shrinkWrap: true,
                     itemBuilder: (BuildContext context,int index){
                       Orders item=snapshot.data[index];
                       return Card(
-                        color: Colors.blueGrey,
+                        color: Colors.grey[600],
                         shadowColor: Colors.black,
                         child: Container(
-                          width: 180,
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                               children:
                               [
 
                                 Text(
-                                  item.orderId,
+                                  item.source,
                                   style:
                                   TextStyle(fontSize: 20.0,color: Colors.white),
                                 ),
                                 SizedBox(
                                   height: 5,
                                 ),
-                                Text(item.genrDate,style: TextStyle(color: Colors.white),),
+                                Text(item.customer,style: TextStyle(color: Colors.white),),
                                 Padding(
                                   padding: const EdgeInsets.all(8.0),
-                                  child: Text(item.custName,
+                                  child: Text(item.seller,
                                   style:
                                     TextStyle(fontSize:15,color: Colors.white)),
                                 ),
-                                Container(
-                                  width: 80,
-                                  child: RaisedButton(
-                                    hoverColor: Colors.blueGrey,
-                                    onPressed: () {},
-                                    color: Colors.black,
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius. circular(20)),
-                                    child: Text("Accept",style:
-                                      TextStyle(color: Colors.white,fontWeight: FontWeight.bold)),
-                                  ),
-                                )
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(item.seller,
+                                  style:
+                                    TextStyle(fontSize:15,color: Colors.white)),
+                                ),
+                                
                               ],
                             ),
                         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
-import 'drawer.dart';
-import 'homeScreen.dart';
-
+import 'package:sellerapp/DashboardMenu.dart';
 void main() {
-  runApp(MaterialApp(home: SellerApp(),));
+  runApp(MyApp());
 }
 
-class SellerApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'seller app',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+ 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    
     return Scaffold(
-      body: Stack(
-          children: [
-            DrawerScreen(),
-            HomeScreen(),
-            //MyHomePage(),
-          ]
-      ),
+      body: MenuDashboard(),
     );
   }
 }
